a887c79bf0b2224b9ee3f8710440d28a303a2299
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-27 17:03:54 +0530

    Add files via upload

---
+++ constraints.sql
@@ -0,0 +1,24 @@
+CREATE TABLE temp1(
+	id INT UNIQUE 
+);
+
+INSERT INTO temp1 VALUES (101);
+
+SELECT * FROM temp1;
+
+CREATE TABLE temp2(
+	id INT,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(20),
+    PRIMARY KEY (id)
+    );
+    
+    
+CREATE TABLE emp(
+	id INT,
+    salary INT DEFAULT 25000);
+    
+INSERT INTO emp (id)  VALUES (101);
+
+SELECT * FROM emp;
